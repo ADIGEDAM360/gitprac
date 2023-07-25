2e86df19cda47ed838562bb70aab4f56b87bb702
aditya_gedam <adigedam360@gmail.com>
2023-07-25 23:00:14 +0530

    third commit

--- app.py
+++ app.py
@@ -4,11 +4,15 @@ def outer(func):
         func()
         print("parameterized function exceuted")
     return inner()
-def outer(func):
-    def inner():
+def outer2(func):
+    def inner2():
         print("inside the inner functuion")
         func()
         print("parameterized function exceuted")
-    return inner()
+    return inner2()
+
+@outer
+def trail():
+    pass
  
  
